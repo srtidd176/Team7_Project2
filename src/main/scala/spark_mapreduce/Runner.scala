913355e828a8f1115d85f9b41feec399b2649aae
srtidd23 <sean.tidd@revature.net>
2020-11-11 17:23:02 -0500

    creaeted base design

---
+++ src/main/scala/spark_mapreduce/Runner.scala
@@ -0,0 +1,24 @@
+package spark_mapreduce
+
+object Runner {
+
+  val sparkEmoji: SparkEmoji = new SparkEmoji("local[4]")
+
+  def main(args: Array[String]): Unit = {
+    if(args.length < 1){
+      println("Usage: <function number> <arg1> <arg2> ....")
+      System.exit(-1)
+    }
+
+    args match {
+        // Upload and perform the demoQuery TODO delete this test
+      case Array(func, lenMax) => if(func == "demoQuery") {
+        sparkEmoji.uploadJSON("people.json",true)
+        sparkEmoji.demoQuery(lenMax.toInt)
+      }
+        // Catch any other cases
+      case _ =>
+    }
+
+  }
+}
