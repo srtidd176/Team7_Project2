d768db0ba422af684e2ed068ae841c2e38e89d3f
srtidd23 <sean.tidd@revature.net>
2020-11-13 16:29:12 -0500

    identified single emojis

--- src/main/scala/spark_mapreduce/SparkEmoji.scala
+++ src/main/scala/spark_mapreduce/SparkEmoji.scala
@@ -6,6 +6,7 @@ Each analytical question receives it's own method.
 package spark_mapreduce
 
 import org.apache.spark.sql
+import org.apache.spark.sql.functions.{explode, not}
 import org.apache.spark.sql.{SparkSession, functions}
 
 
@@ -55,16 +56,34 @@ class SparkEmoji(master: String) {
   def emojiValue(): Unit ={
     import spark.implicits._
 
-    val emojiRegex = "(\u00a9|\u00ae|[\u2000-\u3300]|\uD83C[\uD000-\uDFFF]|\uD83D[\uD000-\uDFFF]|\uD83E[\uD000-\uDFFF])"
+    val emojiRegexSplit = "\u00a9|\u00ae|[\u2000-\u3300]|[\ud83c\ud000-\ud83c\udfff]|[\ud83d\ud000-\ud83d\udfff]|[\ud83e\ud000-\ud83e\udfff]"
+    val emojiRegexSingle = "^\u00a9$|^\u00ae$|^[\u2000-\u3300]$|^[\ud83c\ud000-\ud83c\udfff]$|^[\ud83d\ud000-\ud83d\udfff]$|^[\ud83e\ud000-\ud83e\udfff]$"
     uploadJSON("twitter.json",true)
 
+
     val dfEmojiSplit = dfRaw.select("id", "text")
-      .withColumn("text", functions.explode(functions.split($"text", " "))) //split by spaces and explode
-      .filter($"text" rlike "(^\u00a9|^\u00ae|^[\u2000-\u3300])") // filter out everything that is not an emoji
+      .withColumn("text", functions.explode(functions.split($"text", "\\s"))) //split by spaces and explode
+      .filter($"text" rlike emojiRegexSplit) // filter out everything that is not emoji-like
 
-    dfRaw.printSchema()
+    val condition = $"text" rlike emojiRegexSingle //filter out everything that is not a single emoji
+    val dfEmojiSingle = dfEmojiSplit.filter(condition) //single emojis
+    val dfEmojiGroups = dfEmojiSplit.filter(not(condition)) //concatenated emojis
 
-    dfEmojiSplit.show()
+    dfEmojiGroups.show()
+    dfEmojiSingle.show()
+
+    //TODO Break up the emoji groups
+    //dfEmojiGroups.foreach( row => row.get(1))
+
+    //dfEmojiGroups.withColumn("text", functions.explode(functions.split($"text", emojiRegexSplit)))
+      //.filter(condition) //collect single emojis
+      //.filter($"text" rlike "(?=[^?])") //ignore any unknown items
+      //.show()
+
+
+    //dfRaw.printSchema()
+
+    //dfEmojiSplit.show()
   }
 
 
