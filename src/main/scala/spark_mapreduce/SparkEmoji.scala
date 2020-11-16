b13fbc8e8a5c834c9510c84c9b85b1ef9804b56c
srtidd23 <sean.tidd@revature.net>
2020-11-16 17:51:59 -0500

    merged with zach branch

--- src/main/scala/spark_mapreduce/SparkEmoji.scala
+++ src/main/scala/spark_mapreduce/SparkEmoji.scala
@@ -32,10 +32,33 @@ class SparkEmoji(master: String) {
    * @param multiline boolean to indicate whether the JSON objects are multilined or not
    */
   def uploadJSON(path: String, multiline: Boolean, stream: Boolean): Unit={
-    dfRaw = spark.read.option("multiline", multiline).json(path)
+    import spark.implicits._
+    val df = spark.read.option("multiline", multiline).json(path)
 
-    if(stream){
-      dfStreamRaw =  spark.readStream.schema(dfRaw.schema).json(path)
+      if(!stream) {
+        //getting the tweet DF
+        val splitDfTweet = df.select("data").withColumn("data", functions.explode($"data"))
+        val dfTweet = splitDfTweet.select("data.*")
+        val dfTweetFlatten = dfTweet.toDF("author_id", "time", "id", "lang", "public_metrics", "text")
+        val dfTweetFull = dfTweetFlatten.select("id", "author_id", "time", "lang", "public_metrics.*", "text")
+        val dfTweetFullFlatten = dfTweetFull.toDF("tweet_id", "author_id", "time", "lang", "like_count", "quote_count", "reply_count", "retweet_count", "text")
+        //dfTweetFullFlatten.show()
+
+        //getting the author DF
+        val splitDfAuth = df.select("includes.*")
+        val dfAuthExpl = splitDfAuth.select("users").withColumn("users", functions.explode($"users"))
+        val dfAuth = dfAuthExpl.select("users.id", "users.name", "users.username", "users.public_metrics")
+        val dfAuthFlatten = dfAuth.toDF("id", "name", "username", "public_metrics")
+        val dfAuthFull = dfAuthFlatten.select("id", "name", "username", "public_metrics.followers_count", "public_metrics.following_count", "public_metrics.tweet_count", "public_metrics.listed_count")
+        val dfAuthFullFlatten = dfAuthFull.toDF("user_id", "name", "username", "followers_count", "following_count", "tweet_count", "listed_count")
+        //dfAuthFullFlatten.show()
+
+        //inner joins the two df
+        dfRaw = dfTweetFullFlatten.join(dfAuthFullFlatten, dfTweetFullFlatten("author_id") === dfAuthFullFlatten("user_id"))
+      }
+
+    else{
+      dfStreamRaw =  spark.readStream.schema(df.schema).json(path)
     }
 
   }
@@ -61,7 +84,7 @@ class SparkEmoji(master: String) {
     val emojiRegexLike = "\u00a9|\u00ae|[\u2000-\u3300]|[\ud83c\ud000-\ud83c\udfff]|[\ud83d\ud000-\ud83d\udfff]|[\ud83e\ud000-\ud83e\udfff]" //Identify "emoji-like" words
     val emojiRegexSingle = "^\u00a9$|^\u00ae$|^[\u2000-\u3300]$|^[\ud83c\ud000-\ud83c\udfff]$|^[\ud83d\ud000-\ud83d\udfff]$|^[\ud83e\ud000-\ud83e\udfff]$" //Identify unique emojis
 
-    val dfEmojiSplit = inputDF.select("id", "text")
+    val dfEmojiSplit = inputDF.select("text_id", "text")
       .withColumn("text", functions.explode(functions.split($"text", "\\s"))) //split by spaces and explode
       .filter($"text" rlike emojiRegexLike) // filter out everything that is not emoji-like
 
