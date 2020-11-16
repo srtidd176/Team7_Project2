aea8f22c795c10dc2cace3c365a18ba452681c30
srtidd23 <sean.tidd@revature.net>
2020-11-16 15:23:41 -0500

    made an emojivaluesStream

--- src/main/scala/spark_mapreduce/SparkEmoji.scala
+++ src/main/scala/spark_mapreduce/SparkEmoji.scala
@@ -69,11 +69,8 @@ class SparkEmoji(master: String) {
     val dfEmojiSingle = dfEmojiSplit.filter(condition) //single emojis
     val dfEmojiGroups = dfEmojiSplit.filter(not(condition)) //concatenated emojis
 
-    dfEmojiGroups.show()
-    dfEmojiSingle.show()
-
-    emojiDF = dfEmojiSingle
-    emojiDF.show()
+    //dfEmojiGroups.show()
+    //dfEmojiSingle.show()
 
     dfEmojiSingle //return
 
@@ -84,6 +81,35 @@ class SparkEmoji(master: String) {
       .show(50)
     */
   }
+
+  def emojiValueStream(inputDF: DataFrame, seconds: Int): Boolean ={
+    import spark.implicits._
+
+    val emojiRegexLike = "\u00a9|\u00ae|[\u2000-\u3300]|[\ud83c\ud000-\ud83c\udfff]|[\ud83d\ud000-\ud83d\udfff]|[\ud83e\ud000-\ud83e\udfff]" //Identify "emoji-like" words
+    val emojiRegexSingle = "^\u00a9$|^\u00ae$|^[\u2000-\u3300]$|^[\ud83c\ud000-\ud83c\udfff]$|^[\ud83d\ud000-\ud83d\udfff]$|^[\ud83e\ud000-\ud83e\udfff]$" //Identify unique emojis
+
+    val dfEmojiSplit = inputDF.select("data.id", "data.text")
+      .withColumn("text", functions.explode(functions.split($"text", "\\s"))) //split by spaces and explode
+      .filter($"text" rlike emojiRegexLike) // filter out everything that is not emoji-like
+
+    val condition = $"text" rlike emojiRegexSingle //filter out everything that is not a single emoji
+    val dfEmojiSingle = dfEmojiSplit.filter(condition) //single emojis
+    val dfEmojiGroups = dfEmojiSplit.filter(not(condition)) //concatenated emojis
+
+    dfEmojiSingle.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+      .awaitTermination(seconds*1000)
+
+    //TODO Break up the emoji groups
+    /*val emojiGroupsRDD = dfEmojiGroups//.withColumn("text", functions.explode(functions.split($"text", emojiRegexSplit)))
+      .filter(condition) //collect single emojis
+      .filter($"text" rlike "(?=[^?])") //ignore any unknown items
+      .show(50)
+    */
+  }
+
 
 
 
